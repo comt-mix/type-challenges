06595649719deb612c3d7907d3dfc4d235721d12
comt-mix <comt.mix@gmail.com>
2023-06-07 10:15:31 +0900

    533. Concat(easy)
    any에서 unknown으로 타입 수정

--- questions/00533-easy-concat/template.ts
+++ questions/00533-easy-concat/template.ts
@@ -1,6 +1,6 @@
 // type Concat<T, U> = any
 
-type Concat<T extends any[], U extends any[]> = [...T, ...U];
+type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U];
 
 // 내 풀이
 // type Concat<T extends number[], U extends number[]> = [...T, ...U];
