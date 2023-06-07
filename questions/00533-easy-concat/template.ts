5b641746f9a6776bfdfa14099fd62ac3f1ecad99
comt-mix <comt.mix@gmail.com>
2023-06-07 10:04:43 +0900

    533. Concat(easy)

--- questions/00533-easy-concat/template.ts
+++ questions/00533-easy-concat/template.ts
@@ -1,1 +1,6 @@
-type Concat<T, U> = any
+// type Concat<T, U> = any
+
+type Concat<T extends any[], U extends any[]> = [...T, ...U];
+
+// 내 풀이
+// type Concat<T extends number[], U extends number[]> = [...T, ...U];
