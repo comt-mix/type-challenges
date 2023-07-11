bfb1a37f949b2a48ce2a0cd9217f1696520b6ba2
salee <sanga0108@smartscore.kr>
2023-07-11 12:59:23 +0900

    20. Promise.all (medium)

--- questions/00020-medium-promise-all/template.ts
+++ questions/00020-medium-promise-all/template.ts
@@ -1,1 +1,9 @@
-declare function PromiseAll(values: any): any
+// declare function PromiseAll(values: any): any
+
+// 내 풀이
+// declare function PromiseAll(values: any): new Promise<T: values>
+
+// 정답
+declare function PromiseAll<T extends unknown[]>(
+  values: readonly [...T]
+): Promise<{ [K in keyof T]: T[K] extends Promise<infer R> ? R : T[K] }>
