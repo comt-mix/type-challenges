04ca14882c5f74a512ce08c61221056a13301dba
salee <sanga0108@smartscore.kr>
2023-07-11 09:55:03 +0900

    16. Pop (medium)

--- questions/00016-medium-pop/template.ts
+++ questions/00016-medium-pop/template.ts
@@ -1,1 +1,7 @@
-type Pop<T extends any[]> = any
+// type Pop<T extends any[]> = any
+
+// 내 풀이
+// type Pop<T extends unknown[]> = [...y, x] extends T ? x : never
+
+// 정답
+type Pop<T extends unknown[]> = T extends [...infer L, infer R] ? L : never
